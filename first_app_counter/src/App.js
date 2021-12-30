a37b10ce11ea0942baae5094ac64a98c612628bb
unknown <mizanur35-2844@diu.edu.bd>
2021-12-30 21:01:59 +0600

    add button functionality added :boom:

--- first_app_counter/src/App.js
+++ first_app_counter/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Counter from './components/counter.component';
+import Navbar from './components/navbar.component';
 
-function App() {  
-	return (
-		<React.Fragment>
-			<Counter />
-		</React.Fragment>
-	);
+class App extends Component {
+
+	state = {
+		counters: [1]
+	}
+
+	handleAdd = () => {
+		const counters  = [...this.state.counters];
+		counters.push(1);
+
+		this.setState({ counters });
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar />
+				<button className='btn btn-primary mt-2' onClick={this.handleAdd}>Add</button>
+				
+				{
+					this.state.counters.map((counter, index) => (
+						<>
+							<Counter key={index} />
+							
+						</>
+					))
+				}
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- first_app_counter/src/components/counter.component.js
+++ first_app_counter/src/components/counter.component.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 	state = {
-		count: 0,
+		count: 1,
 	};
 
 	handleIncrement = () => {
-		const { count } = this.state;
-		this.setState({
-			count: count + 1,
-		});
+		this.setState({ count: this.state.count + 1 });
 	};
 
 	handleDecrement = () => {
 		const { count } = this.state;
-		if (count > 0) this.setState({ count: count - 1 });
+		if (count > 1) this.setState({ count: count - 1 });
 	};
 
 	handleReset = () => {
@@ -24,17 +21,26 @@ class Counter extends Component {
 	render() {
 		return (
 			<>
-				<div className="container border border-success rounded mt-4">
-					<h1 className='text-center'>Counter App</h1>
+				<div className="container mt-4">
+					<button
+						disabled={this.state.count === 1}
+						className="btn btn-primary"
+						onClick={this.handleDecrement}
+					>
+						-
+					</button>
+
+					<span className="m-4">{this.state.count}</span>
+
 					<button className="btn btn-primary" onClick={this.handleIncrement}>
 						+
 					</button>
-					<span className="m-4">{this.state.count}</span>
-					<button className="btn btn-primary" onClick={this.handleDecrement}>
-						-
-					</button>
-					<button className="btn btn-danger m-4" onClick={this.handleReset}>
+
+					<button className="btn btn-info m-4" onClick={this.handleReset}>
 						Reset
+					</button>
+					<button className="btn btn-danger" onClick={this.handleRemove}>
+						Remove
 					</button>
 				</div>
 			</>
