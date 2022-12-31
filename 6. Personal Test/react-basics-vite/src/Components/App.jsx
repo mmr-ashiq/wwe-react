311489bae673305b2f69df833d24b234a58871b4
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2023-01-01 00:05:42 +0600

    box component finished in class based

--- 6. Personal Test/react-basics-vite/src/Components/App.jsx
+++ 6. Personal Test/react-basics-vite/src/Components/App.jsx
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import Box from './Box';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
 	state = {
-		boxes: [1, 2, 3, 4, 5],
+		boxes: [],
+	};
+
+	createBox = () => {
+		const { boxes } = this.state;
+		this.setState({ boxes: [...boxes, 0] });
+	};
+
+	onDelete = (id) => {
+		const { boxes } = this.state;
+		const newBoxes = boxes.filter((box, index) => index !== id);
+		this.setState({ boxes: newBoxes });
+	};
+
+	onIncrease = (id) => {
+		const { boxes } = this.state;
+		const newBoxes = boxes.map((box, index) => {
+			if (index === id) {
+				return box + 1;
+			}
+			return box;
+		});
+		this.setState({ boxes: newBoxes });
+	};
+
+	onDecrease = (id) => {
+		const { boxes } = this.state;
+		const newBoxes = boxes.map((box, index) => {
+			if (index === id && box > 0) {
+				return box - 1;
+			}
+			return box;
+		});
+		this.setState({ boxes: newBoxes });
 	};
 
 	render() {
 		return (
-			<React.Fragment className='m-4'>
-				{this.state.boxes.map((number) => {
-					return (
-						<React.Fragment key={number}>
-							<Box />
-							<br />
-						</React.Fragment>
-					);
-				})}
+			<React.Fragment>
+				<button className='btn btn-primary m-2' onClick={this.createBox}>Add New Box</button>
+				{this.state.boxes.map((number, index) => (
+					<React.Fragment key={index}>
+						<Box num={number} id={index} onDelete={this.onDelete} onIncrease={this.onIncrease} onDecrease={this.onDecrease} />
+						<br />
+					</React.Fragment>
+				))}
 			</React.Fragment>
 		);
 	}
--- 6. Personal Test/react-basics-vite/src/Components/Box.jsx
+++ 6. Personal Test/react-basics-vite/src/Components/Box.jsx
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
 
 export default class Box extends Component {
-	state = {
-		count: 0,
+	constructor(props) {
+		super(props);
+	}
+
+	delete = () => {
+		const { id } = this.props;
+		this.props.onDelete(id);
 	};
 
 	increase = () => {
-		const newNumber = this.state.count + 1;
-		const newState = { count: newNumber };
-		this.setState(newState);
+		this.props.onIncrease(this.props.id);
 	};
 
-	decrease() {
-		const newNumber = this.state.count - 1;
-		const newState = { count: newNumber };
-		this.setState(newState);
-	}
+	decrease = () => {
+		this.props.onDecrease(this.props.id);
+	};
 
 	render() {
 		return (
 			<div className="container">
-				<button className="btn btn-danger" onClick={this.decrease}>
-					-
+				<button className="btn btn-info m-2" onClick={this.decrease}>
+					Decrement
 				</button>
-				{this.state.count}
-				<button className="btn btn-primary" onClick={this.increase}>
-					+
+				<span className="m-2">{this.props.num}</span>
+				<button className="btn btn-success m-2" onClick={this.increase}>
+					Increment
+				</button>
+				<button className="btn btn-outline-danger m-2" onClick={this.delete}>
+					Delete
 				</button>
 			</div>
 		);
